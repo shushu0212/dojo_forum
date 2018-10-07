3e70b809b9a72df8bf3e3b3692232066f87d0cf8
shushu <shushu0212@gmail.com>
2018-10-08 07:44:13 +0800

    use devise to create users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007232426) do
+ActiveRecord::Schema.define(version: 20181007233653) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,23 @@ ActiveRecord::Schema.define(version: 20181007232426) do
     t.integer "viewed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "intro"
+    t.string "avatar"
+    t.integer "topics_count", default: 0
+    t.integer "comments_count", default: 0
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
