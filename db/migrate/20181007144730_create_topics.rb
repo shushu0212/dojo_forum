c681be567e19295d0e0f1b2ed4362d6f749f3187
shushu <shushu0212@gmail.com>
2018-10-07 22:56:14 +0800

    create topics model

---
+++ db/migrate/20181007144730_create_topics.rb
@@ -0,0 +1,12 @@
+class CreateTopics < ActiveRecord::Migration[5.1]
+  def change
+    create_table :topics do |t|
+      t.string :title
+      t.text :content
+      t.integer :category_id
+      t.integer :comments_count, default: 0
+      t.integer :viewed_count, default: 0
+      t.timestamps
+    end
+  end
+end
