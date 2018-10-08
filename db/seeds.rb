80aa8463a61a5a4ea171b64c6ee57a1946e56003
shushu <shushu0212@gmail.com>
2018-10-08 23:55:16 +0800

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Category
+
+Category.destroy_all
+
+category_list =[
+  { name: "分類1" },
+  { name: "分類2" },
+  { name: "分類3" },
+  { name: "分類4" },
+  { name: "分類5" }
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
+
+
+# Default admin
+
+User.create(email: "admin@gmail.com", password: "12345678", role: "admin")
+puts "Default admin created!"
