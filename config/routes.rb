0e7bdb1f21054619c1bb071e2929512e3a553b7e
shushu <shushu0212@gmail.com>
2018-10-10 10:43:47 +0800

    set basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :topics do
+    resources :comments, only: [:create, :destroy]
+    
+    collection do
+      get :feeds
+    end
+
+    member do
+      post :favorite
+      post :unfavorite
+    end
+  end
+  
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :categories, only: :show
+  
+  root "topics#index"
+
+  namespace :admin do
+    resources :categories
+    resources :users
+    root "categories#index"
+  end
 end
