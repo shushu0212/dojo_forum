3cca9244951b94eb520e912f6b03994c8caa5d5c
shushu <shushu0212@gmail.com>
2018-10-10 10:12:05 +0800

    add user_id to topics table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181008155746) do
+ActiveRecord::Schema.define(version: 20181009162311) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20181008155746) do
     t.integer "viewed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
