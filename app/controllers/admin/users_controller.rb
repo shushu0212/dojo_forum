346cb4241124a4871cd67b206e43283ce6b53fa8
shushu <shushu0212@gmail.com>
2018-10-12 08:45:13 +0800

    ceate amdin user list page

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,36 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+  before_action :set_user, only: [:update]
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    if @user.update(user_params_role)
+      redirect_to admin_users_path
+      flash[:notice] = "user's role was successfully updated"
+    else
+      @users = User.all
+      render :index
+    end
+  end
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "Not allow!"
+      redirect_to root_path
+    end
+  end
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
+
+  def user_params_role
+    params.require(:user).permit(:role.name)
+  end
+end
