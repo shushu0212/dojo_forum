b8a75d4fe727971db19e9c60f4a430d5902bd993
shushu <shushu0212@gmail.com>
2018-10-14 17:58:31 +0800

    add topic's comment count and user's comment count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :topic
+  belongs_to :user, counter_cache: :comments_count
+  belongs_to :topic, counter_cache: :comments_count
 end
