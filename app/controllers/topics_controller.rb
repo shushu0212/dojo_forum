0d6f6ebdb84c73505a8b853dce565d535ee84c56
shushu <shushu0212@gmail.com>
2018-10-14 17:45:57 +0800

    add viewed count with user browse topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,22 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show]
+
   def index
     @topics = Topic.all
+  end
+
+  def show
+    @topic.viewed_count += 1
+    @topic.save!
+  end
+
+  private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:viewed_count)
   end
 end
