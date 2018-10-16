5488608cdafb493237734f8cb4834dc179d0c2a3
shushu <shushu0212@gmail.com>
2018-10-16 08:36:36 +0800

    ceate topics#new (can create topic include title, content, category)

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show]
+  before_action :authenticate_user!, except: :index
 
   def index
     @topics = Topic.all
@@ -10,6 +11,21 @@ class TopicsController < ApplicationController
     @topic.save!
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = current_user.topics.build(topic_params)
+    if @topic.save
+      flash[:notice] = "topic was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "topic was failed to create"
+      render :new
+    end
+  end
+
   private
 
   def set_topic
@@ -17,6 +33,6 @@ class TopicsController < ApplicationController
   end
 
   def topic_params
-    params.require(:topic).permit(:viewed_count)
+    params.require(:topic).permit(:title, :content, :viewed_count, category_ids: [])
   end
 end
