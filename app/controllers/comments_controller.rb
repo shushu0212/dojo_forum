aa1b7b4f241b96d70f3b460f3558b9695a3be9cc
shushu <shushu0212@gmail.com>
2018-10-20 10:39:18 +0800

    set routes and controller for comments#edit and comments#update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_topic, only: [:create, :destroy]
+  before_action :set_topic, only: [:create, :destroy, :edit, :update]
+  before_action :set_comment, only: [:destroy, :edit, :update]
   
   def create
     @comment = @topic.comments.build(comment_params)
@@ -10,11 +11,27 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-
     if current_user.admin? || @comment.user.email == current_user.email
       @comment.destroy
       redirect_to topic_path(@topic)
+    end
+  end
+
+  def edit
+    unless @comment.user == current_user
+      flash[:alert] = "非本人，無法編輯!"
+      redirect_to topic_path(@topic)
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      @topic.update(last_commnet_created_at: @comment.created_at)
+      flash[:notice] = "comment was successfully updated"
+      redirect_to topic_path(@topic)
+    else
+      flash[:alert] = @comment.errors.full_messages.to_sentence
+      render :edit
     end
   end
 
@@ -22,6 +39,10 @@ class CommentsController < ApplicationController
 
   def set_topic
     @topic = Topic.find(params[:topic_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :topics do
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy, :edit, :update]
     
     collection do
       get :feeds
