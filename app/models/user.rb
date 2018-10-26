87933a7f2c69304b1ef7012e9fe225f41e4d59c4
shushu <shushu0212@gmail.com>
2018-10-26 21:18:39 +0800

    add user authentication_token for post api

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
+  before_create :generate_authentication_token
   
   has_many :topics
   has_many :comments, dependent: :destroy
@@ -43,6 +44,10 @@ class User < ApplicationRecord
   def all_friends 
     friends = self.friendings + self.frienders
     return friends.uniq
+  end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025004631) do
+ActiveRecord::Schema.define(version: 20181026131357) do
 
   create_table "audiences", force: :cascade do |t|
     t.string "name"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20181025004631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role_id", default: 2
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
