f5014a460dff03d56947e47d81f068d2f04cd61a
shushu <shushu0212@gmail.com>
2018-10-26 22:38:54 +0800

    add update action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::PostsController < ApiController
+  before_action :set_topic, except: [:index, :create]
   before_action :authenticate_user!, except: :index
+  
   def index
     @posts = Topic.all
     render json: {
@@ -34,9 +36,34 @@ class Api::V1::PostsController < ApiController
       }
     end
   end
+
+  def update
+    if @post.user == current_user
+      if @post.update(topic_params)
+        render json: {
+          message: "Topic updated successfully!",
+          result: @post
+        }
+      else
+        render json: {
+          errors: @post.errors
+        }
+      end
+    else
+      render json: {
+        message: "非本人文章!"
+    }
+    end
+  end
+
+
   
   private
   
+  def set_topic
+    @post = Topic.find(params[:id])
+  end
+
   def topic_params
     params.permit(:title, :content, :image, :audience_id, category_ids: [])
   end
