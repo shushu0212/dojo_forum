b337f0092e3396cf2ccca44cbdd2f2cad2aaefd0
shushu <shushu0212@gmail.com>
2018-10-26 16:57:23 +0800

    add show action for post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,18 @@ class Api::V1::PostsController < ApiController
       data: @posts
     }
   end
+
+  def show
+    @post = Topic.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        data: @post
+      }
+    end
+  end
 end
