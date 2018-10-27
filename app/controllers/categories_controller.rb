490b7771062f98a8a40761163321d9c67025b53c
shushu <shushu0212@gmail.com>
2018-10-27 22:52:47 +0800

    topic order by id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @topics = @category.topics.page(params[:page]).per(20)
+    @topics = @category.topics.page(params[:page]).per(20).order(:id)
   end
 end
